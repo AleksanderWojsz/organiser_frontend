7ca3f8b427391742d4a88f2bdd788b0e89b71403
Aleksander Wojsz <you@example.com>
2024-08-24 19:30:02 +0200

    added authorization
    added authorization
    
    should have commited .env.local earlier

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,35 @@
 <script setup>
-
-import { RouterLink , useRoute } from 'vue-router';
-import { RouterView } from "vue-router";
+import { useAuth0 } from '@auth0/auth0-vue';
+import { ref } from 'vue'
 import NavigationBar from "@/components/NavigationBar.vue";
-import { ref, onMounted } from "vue";
+
+const auth0 = useAuth0();
+const { loginWithRedirect, logout } = useAuth0();
+const isAuthenticated = ref(auth0.isAuthenticated);
+const isLoading = ref(auth0.isLoading);
+const user = ref(auth0.user);
+
+const login = () => {
+    loginWithRedirect();
+}
+
+const logOut = () => {
+  logout({ logoutParams: { returnTo: window.location.origin } });
+}
 
 const user_id = 1;
 
 </script>
 
 <template>
+    <button v-if="!isAuthenticated && !isLoading" @click="login">Log in</button>
+    <button v-if="isAuthenticated && !isLoading" @click="logOut">Log out</button>
 
-  <NavigationBar></NavigationBar>
-  <RouterView v-bind:user_id="user_id"></RouterView>
+    <div v-if="isAuthenticated && !isLoading">
+        <p>Unikalne id: {{user.sub}}</p>
+    </div>
 
+
+    <NavigationBar></NavigationBar>
+    <RouterView v-bind:user_id="user_id"></RouterView>
 </template>
-
