fe2db12f299b55800c79eeb4d2324dc815b73de0
Aleksander <Pieczalek@gmail.com>
2024-08-30 15:39:49 +0200

    deleted env variables

--- src/main.js
+++ src/main.js
@@ -2,37 +2,22 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css'
 import router from './router/index.js'
-import { createAuth0 } from '@auth0/auth0-vue';
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
+// If lost all keys can be found under
+// https://console.firebase.google.com/u/0/project/organiser-850a0/settings/general
 const firebaseConfig = {
-  apiKey: "AIzaSyAjT98XnPg54d88RHNayJ_CPCRBpbSJHi8",
-  authDomain: "organiser-850a0.firebaseapp.com",
-  projectId: "organiser-850a0",
-  storageBucket: "organiser-850a0.appspot.com",
-  messagingSenderId: "963250076189",
-  appId: "1:963250076189:web:35ba8967d36d61c54ae4f4"
+  apiKey: import.meta.env.VITE_API_API_KEY,
+  authDomain: import.meta.env.VITE_API_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_API_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_API_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_API_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_API_APP_ID,
 };
 
 initializeApp(firebaseConfig);
 
-
 const app = createApp(App);
-
 app.use(router);
-app.use(
-  createAuth0({
-    domain: import.meta.env.VITE_AUTH0_DOMAIN, // Defined in `.env.local`
-    clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
-    authorizationParams: {
-      redirect_uri: window.location.origin
-    }
-  })
-);
-
 app.mount('#app')
